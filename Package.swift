598ba456790f6be9579d4d0021190b8cab0e7fac
Nick Servidio <servidionicholas@gmail.com>
2021-07-23 12:13:08 -0700

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "BKPasscodeView",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "BKPasscodeView",
+            targets: ["BKPasscodeView"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/nservidio/AFViewShaker",
+            .branch("spm")
+        )
+    ],
+    targets: [
+        .target(
+            name: "BKPasscodeView",
+            path: "BKPasscodeView"
+        )
+    ]
+)
